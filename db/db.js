fd46155a44d01dd6adae40db6f2281e27f3b81e1
Juan Weffer <147261579+geek-lock@users.noreply.github.com>
2024-07-09 18:26:59 -0300

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,27 +1,28 @@
 require('dotenv').config();
+const mysql = require('mysql2');
 
-const mySql = require('mysql2');
+// Crear un pool de conexiones
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10, // Número máximo de conexiones en el pool
+  queueLimit: 0 // Sin límite en la cola de conexiones
+});
 
-const connection = mySql.createConnection(
-    {
-        host : process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password : process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
-    });
+// Conectar al pool de conexiones
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("ERROR conectando a la base de datos", err);
+    return;
+  }
 
+  console.log("Conectado EXITOSAMENTE a la base de datos");
 
-    connection.connect((err) => 
-    {
-        if(err)
-        {
-            console.error("ERROR conectando a la base de datos", err);
-            return;
-        }
+  // Liberar la conexión de vuelta al pool después de usarla
+  connection.release();
+});
 
-        console.log("Conectado EXITOSAMENTE a la base de datos");
-
-    });
-
-
-module.exports = connection;
+module.exports = pool;
